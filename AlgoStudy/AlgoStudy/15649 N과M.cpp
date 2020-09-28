414f39c50fe18bab67292c20f0b395715d0b5fec
wooniverse7 <wooniverse7@gmail.com>
2020-09-29 01:48:51 +0900

    JustDo

---
+++ AlgoStudy/AlgoStudy/15649 N과M.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+bool c[10]; int a[10];
+
+void go(int index, int n, int m) {
+	if (index == m) {
+		for (int i = 0; i < m; i++) {
+			cout << a[i] << ' ';
+		}
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (c[i]) continue;
+		c[i] = true;
+		a[index] = i;
+		go(index+1, n, m);
+		c[i] = false;
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	go(0, n, m);
+
+	return 0;
+}
--- Test01/Test01/gitTest.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-
-using namespace std;
-
-int main() {
-	cout << "Hello Git";
-
-	return 0;
-}
