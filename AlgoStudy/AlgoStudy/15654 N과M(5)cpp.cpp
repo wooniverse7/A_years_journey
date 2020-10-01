80140fe33fd71dcc47dd1dfd80b1d6527050233e
wooniverse7 <wooniverse7@gmail.com>
2020-10-02 03:15:26 +0900

    commit

--- AlgoStudy/AlgoStudy/15654 N과M(5)cpp.cpp
+++ AlgoStudy/AlgoStudy/15654 N과M(5)cpp.cpp
@@ -1,30 +1,34 @@
-//컴파일 순서 이해
 #include<iostream>
-
+#include<algorithm>
 using namespace std;
 
-bool c[10]; int a[10];
-
+bool c[10]; int a[10]; int k[10];
 void go(int index, int n, int m) {
 	if (index == m) {
 		for (int i = 0; i < m; i++) {
-			cout << a[i] << ' ';
+			cout << k[a[i]] << ' ';		//a배열이 1~n까지 순열이니까 그에 맞춰서 출력
 		}
 		cout << '\n';
+		return;
 	}
 
-	for (int i = 1; i <= n; i++) {
+	for (int i = 0; i < n; i++) {
 		if (c[i]) continue;
 		c[i] = true;
 		a[index] = i;
-		go(index+1, n, m);		//for문을 다 컨티뉴하면 go로 돌아오는 이유. index가 1씩 줄어드는 이유.
+		go(index + 1, n, m);
 		c[i] = false;
 	}
 }
 
+
 int main() {
 	int n, m;
 	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> k[i];
+	}
+	sort(k, k + n);
 	go(0, n, m);
 
 	return 0;
